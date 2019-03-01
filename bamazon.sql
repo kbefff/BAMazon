ae67e9af3c90b6ff622fe86c45df7443560e922d
kbefff <kbeffel10@me.com>
2019-03-01 12:07:20 -0800

    inserting tables into database

--- bamazon.sql
+++ bamazon.sql
@@ -1,24 +1,41 @@
 
 -- ### Challenge #1: Customer View (Minimum Requirement)
-
+DROP DATABASE IF EXISTS bamazon;
 -- 1. Create a MySQL Database called `bamazon`.
-
+CREATE DATABASE bamazon;
+USE bamazon;
 -- 2. Then create a Table inside of that database called `products`.
-
+CREATE TABLE products(
 -- 3. The products table should have each of the following columns:
 
 --    * item_id (unique id for each product)
-
+    id INTEGER(11) AUTO_INCREMENT NOT NULL,
 --    * product_name (Name of product)
-
+    product_name VARCHAR(30) NOT NULL,
 --    * department_name
-
+    department_name VARCHAR(30) NOT NULL,
 --    * price (cost to customer)
-
+    price DECIMAL NOT NULL,
 --    * stock_quantity (how much of the product is available in stores)
+    stock_quantity INTEGER(10),
+-- Sets id as this table's primary key which means all data contained within it will be unique --
+    PRIMARY KEY (id)
+);
 
 -- 4. Populate this database with around 10 different products. (i.e. Insert "mock" data rows into this database and table).
+    INSERT INTO products (product_name, department_name, price, stock_quantity)
+    VALUES ("Shampoo bar", "Health & Beauty", 12.25, 2),
+    ("Conditioner bar", "Health & Beauty", 14.99, 17),
+    ("Windex", "Home & Kitchen", 6.25, 12),
+    ("Cheerios", "Pantry", 1.99, 202),
+    ("Pancake mix", "Pantry", 3.30, 32),
+    ("Deep cleaning", "Home & Business Services", 100.00, 1),
+    ("one", "two", 1, 1),
+    ("one", "two", 1, 1),
+    ("one", "two", 1, 1),
+    ("one", "two", 1, 1);
 
+SELECT * FROM products;
 -- 5. Then create a Node application called `bamazonCustomer.js`. Running this application will first display all of the items available for sale. Include the ids, names, and prices of products for sale.
 
 -- 6. The app should then prompt users with two messages.
