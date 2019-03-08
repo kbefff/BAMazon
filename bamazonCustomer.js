acbeffa8b0b8b309b16f079da5c2ec0d3e26c2ad
kbefff <kbeffel10@me.com>
2019-03-08 15:25:01 -0800

    some fix

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -30,59 +30,69 @@ connection.connect(function (err) {
 // Running this application will first display all of the Products available for sale. Include the ids, names, and prices of products for sale.
 function readProducts() {
 
-    connection.query('SELECT * FROM products', function (err, results) {
+    connection.query('SELECT * FROM bamazon.products', function (err, results, fields) {
         if (err) throw err;
         console.log('==============================================');
         console.log('');
         console.table(results);
         console.log('');
         console.log('==============================================');
-        // iqnuirer();
+        orderPrompt();
 
-        inquirer
-            .prompt([{
-                    name: "choice",
-                    type: "rawlist",
-                    choices: function () {
-                        var choiceArray = [];
-                        for (var i = 0; i < results.length; i++) {
-                            choiceArray.push(results[i].product_name);
-                        }
-                        return "this is the" + choiceArray;
-                    },
-                    message: "Which product would you like to purchase?"
-                },
-                {
-                    name: 'units',
-                    type: 'input',
-                    message: 'How many units of the product they would you like to buy?'
-                }
-            ])
-            .then(function (answer) {
-                connection.query('SELECT * FROM bamazon.products;', function (err, results, fields) {
-                    var itemIdInt = parseInt(itemID);
-                    var numUnitsInt = parseInt(numUnits);
-                    var chosenProduct;
-                    for (var i = 0; i < results.length; i++) {
-                        if (results[i].item_id === itemIdInt) {
-                            chosenProduct = results[i];
-                        }
-                    }
-
-                    if (chosenProduct.stock_quantity < numUnitsInt) {
-                        console.log("Insufficient quantity in stock!")
-                        prompt();
-                    } else {
-                        fulfillOrder(chosenProduct, numUnitsInt);
-                    }
-
-                });
-
-
-
-            });
     });
 };
+
+function orderPrompt() {
+
+    inquirer
+        .prompt([{
+                name: "choice",
+                type: "rawlist",
+                choices: function () {
+                    var choiceArray = [];
+                    for (var i = 0; i < results.length; i++) {
+                        choiceArray.push(results[i].product_name);
+                    }
+                    return "this is the" + choiceArray;
+                },
+                message: "Which product would you like to purchase?"
+            },
+            {
+                name: 'units',
+                type: 'input',
+                message: 'How many units of the product they would you like to buy?'
+            }
+        ])
+        .then(function (answer) {
+
+
+
+
+        });
+}
+
+function checkItem(itemId, numUnits) {
+
+    connection.query('SELECT * FROM bamazon.products;', function (err, results, fields) {
+        var itemIdInt = parseInt(itemID);
+        var numUnitsInt = parseInt(numUnits);
+        var chosenProduct;
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].item_id === itemIdInt) {
+                chosenProduct = results[i];
+            }
+        }
+
+        if (chosenProduct.stock_quantity < numUnitsInt) {
+            console.log("Insufficient quantity in stock!")
+            prompt();
+        } else {
+            fulfillOrder(chosenProduct, numUnitsInt);
+        }
+
+    });
+
+}
 
 function fulfillOrder(chosenItem, numUnitsInt) {
     let newStock = chosenItem.stock_quantity - numUnitsInt;
