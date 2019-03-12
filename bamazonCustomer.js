f1bbc53fb62d10e82a55b9411af87df964d9c4b0
kbefff <kbeffel10@me.com>
2019-03-11 18:09:52 -0700

    some more customer work

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -26,7 +26,6 @@ connection.connect(function (err) {
 
 // ====================================================================================================
 
-
 // Running this application will first display all of the Products available for sale. Include the ids, names, and prices of products for sale.
 function readProducts() {
 
@@ -38,24 +37,15 @@ function readProducts() {
         console.log('');
         console.log('==============================================');
         orderPrompt();
-
     });
 };
 
 function orderPrompt() {
-
     inquirer
         .prompt([{
-                name: "choice",
-                type: "rawlist",
-                choices: function () {
-                    var choiceArray = [];
-                    for (var i = 0; i < results.length; i++) {
-                        choiceArray.push(results[i].product_name);
-                    }
-                    return "this is the" + choiceArray;
-                },
-                message: "Which product would you like to purchase?"
+                name: "itemID",
+                type: "input",
+                message: "What is the ID of the item you would like to purchase?"
             },
             {
                 name: 'units',
@@ -64,12 +54,10 @@ function orderPrompt() {
             }
         ])
         .then(function (answer) {
-
-
-
-
+            checkItem(answer.itemID, answer.units);
         });
-}
+};
+
 
 function checkItem(itemId, numUnits) {
 
@@ -80,19 +68,16 @@ function checkItem(itemId, numUnits) {
         for (var i = 0; i < results.length; i++) {
             if (results[i].item_id === itemIdInt) {
                 chosenProduct = results[i];
-            }
-        }
-
+            };
+        };
         if (chosenProduct.stock_quantity < numUnitsInt) {
             console.log("Insufficient quantity in stock!")
             prompt();
         } else {
             fulfillOrder(chosenProduct, numUnitsInt);
-        }
-
+        };
     });
-
-}
+};
 
 function fulfillOrder(chosenItem, numUnitsInt) {
     let newStock = chosenItem.stock_quantity - numUnitsInt;
@@ -113,3 +98,28 @@ function fulfillOrder(chosenItem, numUnitsInt) {
         }
     );
 };
+
+
+
+
+
+
+// function fulfillOrder(chosenItem, numUnitsInt) {
+//     let newStock = chosenItem.stock_quantity - numUnitsInt;
+//     let totalPrice = chosenItem.price * numUnitsInt;
+//     connection.query(
+//         "UPDATE products SET ? WHERE ?",
+//         [{
+//                 stock_quantity: newStock
+//             },
+//             {
+//                 item_id: chosenItem.item_id
+//             }
+//         ],
+//         function (error) {
+//             if (error) throw err;
+//             console.log(`You bought ${numUnitsInt} units of the ${chosenItem.product_name}. You spent a total of $${totalPrice}.`);
+//             connection.end();
+//         }
+//     );
+// };
